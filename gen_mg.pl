3b7d5532857b4016f68ec8764f7e737d6e34372c
wang-q <wang-q@outlook.com>
2015-10-22 11:23:33 +0800

    ensure_index() should be ahead of insert()

--- gen_mg.pl
+++ gen_mg.pl
@@ -14,7 +14,6 @@ use Text::CSV_XS;
 
 use MongoDB;
 $MongoDB::BSON::looks_like_number = 1;
-$MongoDB::BSON::utf8_flag_on      = 0;
 use MongoDB::OID;
 
 use AlignDB::IntSpan;
@@ -41,26 +40,26 @@ gen_mg.pl - Generate database from fasta files
 
 =head1 SYNOPSIS
 
-    perl gen_mg.pl -D <directory> [options]
+    perl gen_mg.pl --dir <directory> [options]
       Options:
         --help      -?          brief help message
-        --server    -s  STR     MongoDB server IP/Domain name
-        --port      -P  INT     MongoDB server port
+        --server        STR     MongoDB server IP/Domain name
+        --port          INT     MongoDB server port
         --db        -d  STR     database name
-        --dir       -D  STR     genome files' directory
+        --dir           STR     genome files' directory
         --size      -s  STR     chr.sizes
         --name      -n  STR     common name
         --length        INT     MongoDB has write lock, so we break genome into
-                                pieces and make perl busy
-        --fill          INT
-        --min           INT
-        --parallel  -p  INT     run in parallel mode
+                                pieces and make perl busy, [500_000]
+        --fill          INT     fill gaps smaller than this value, [50]
+        --min           INT     skip pieces smaller than this value, [5000]
+        --parallel      INT     run in parallel mode, [1]
 
-    cd ~/Script/gawm
+    cd ~/Scripts/gawm
     mongo S288c --eval "db.dropDatabase();"
     perl gen_mg.pl -d S288c --dir ~/data/alignment/yeast_combine/S288C --name S288c --parallel 1
 
-    cd ~/Script/alignDB
+    cd ~/Scripts/alignDB
     perl init/init_alignDB.pl -d S288Cvsself
     perl init/gen_alignDB_genome.pl -d S288Cvsself -t "4932,S288C" --dir /home/wangq/data/alignment/yeast65/S288C/  --parallel 4
     perl init/insert_gc.pl -d S288Cvsself --parallel 4
@@ -69,20 +68,52 @@ gen_mg.pl - Generate database from fasta files
 
 GetOptions(
     'help|?' => sub { HelpMessage(0) },
-    's|server=s'   => \( my $server           = $Config->{database}{server} ),
-    'P|port=i'     => \( my $port             = $Config->{database}{port} ),
-    'd|db=s'       => \( my $dbname           = $Config->{database}{db} ),
-    'D|dir=s'      => \( my $dir ),
-    's|size=s'     => \( my $size_file ),
-    'n|name=s'     => \( my $common_name ),
-    'length=i'     => \( my $truncated_length = 500_000 ),
-    'fill=i'       => \( my $fill             = 50 ),
-    'min=i'        => \( my $min_length       = 5000 ),
-    'p|parallel=i' => \( my $parallel         = 1 ),
+    'server=s' => \( my $server           = $Config->{database}{server} ),
+    'port=i'   => \( my $port             = $Config->{database}{port} ),
+    'db|d=s'     => \( my $dbname           = $Config->{database}{db} ),
+    'dir=s'      => \( my $dir ),
+    'size|s=s'   => \( my $size_file ),
+    'name|n=s'   => \( my $common_name ),
+    'length=i'   => \( my $truncated_length = 500_000 ),
+    'fill=i'     => \( my $fill             = 50 ),
+    'min=i'      => \( my $min_length       = 5000 ),
+    'parallel=i' => \( my $parallel         = 1 ),
 ) or HelpMessage(1);
 
 # die unless we got the mandatory argument
 HelpMessage(1) unless defined $dir;
+
+#----------------------------------------------------------#
+# Init
+#----------------------------------------------------------#
+$stopwatch->start_message("Generate database from fasta files");
+
+{
+    $stopwatch->block_message("Init database $dbname");
+    my $db = MongoDB::MongoClient->new(
+        host          => $server,
+        port          => $port,
+        query_timeout => -1,
+    )->get_database($dbname);
+
+    my $coll_chr = $db->get_collection('chromosome');
+    $coll_chr->drop;
+    $coll_chr->ensure_index( { 'chr_name' => 1 } );
+
+    my $coll_seq = $db->get_collection('sequence');
+    $coll_seq->drop;
+    $coll_seq->ensure_index( { 'chr_name'  => 1 } );
+    $coll_seq->ensure_index( { 'chr_start' => 1 } );
+    $coll_seq->ensure_index( { 'chr_end'   => 1 } );
+    $coll_seq->ensure_index( { 'level'     => 1 } );
+
+    my $coll_align = $db->get_collection('align');
+    $coll_align->drop;
+    $coll_align->ensure_index( { 'chr_name'  => 1 } );
+    $coll_align->ensure_index( { 'chr_start' => 1 } );
+    $coll_align->ensure_index( { 'chr_end'   => 1 } );
+    $coll_align->ensure_index( { 'seq_id'    => 1 } );
+}
 
 #----------------------------------------------------------#
 # Search for size and fasta files
@@ -90,7 +121,7 @@ HelpMessage(1) unless defined $dir;
 if ( !defined $size_file ) {
     if ( path( $dir, 'chr.sizes' )->is_file ) {
         $size_file = path( $dir, 'chr.sizes' )->absolute->stringify;
-        printf "--size sest to $size_file\n";
+        printf "--size set to $size_file\n";
     }
     else {
         die "--size chr.sizes is needed\n";
@@ -134,8 +165,6 @@ printf "\n----Total .fa Files: %4s----\n\n", scalar @files;
     close $csv_fh;
 
     $coll_chr->batch_insert( \@chrs, { safe => 1 }, );
-
-    $coll_chr->ensure_index( { 'chr_name' => 1 } );
 
     print "There are @{[$coll_chr->count]} documents in collection chromosome\n";
 }
@@ -181,7 +210,7 @@ my $worker = sub {
 
     my $valid_set = AlignDB::IntSpan->new("1-$chr_length");
     $valid_set->subtract($ambiguous_set);
-    $valid_set = $valid_set->fill( $fill - 1 );    # fill gaps smaller than $fill
+    $valid_set = $valid_set->fill( $fill - 1 );   
 
     print "Valid chromosome region for $chr_name:\n    " . $valid_set->runlist . "\n";
 
@@ -252,27 +281,6 @@ my $worker = sub {
 #----------------------------------------------------------#
 my $mce = MCE->new( max_workers => $parallel, );
 $mce->foreach( \@files, $worker, );    # foreach implies chunk_size => 1.
-
-# indexing
-{
-    my $db = MongoDB::MongoClient->new(
-        host          => $server,
-        port          => $port,
-        query_timeout => -1,
-    )->get_database($dbname);
-
-    my $coll_seq = $db->get_collection('sequence');
-    $coll_seq->ensure_index( { 'chr_name'  => 1 } );
-    $coll_seq->ensure_index( { 'chr_start' => 1 } );
-    $coll_seq->ensure_index( { 'chr_end'   => 1 } );
-    $coll_seq->ensure_index( { 'level'     => 1 } );
-
-    my $coll_align = $db->get_collection('align');
-    $coll_align->ensure_index( { 'chr_name'  => 1 } );
-    $coll_align->ensure_index( { 'chr_start' => 1 } );
-    $coll_align->ensure_index( { 'chr_end'   => 1 } );
-    $coll_align->ensure_index( { 'seq_id'    => 1 } );
-}
 
 $stopwatch->end_message( "All files have been processed.", "duration" );
 
