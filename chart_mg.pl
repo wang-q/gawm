a0876b674462b4270ca27db38eaf1873998dcafc
wang-q <wang-q@outlook.com>
2015-10-23 23:05:39 +0800

    Bring `chart_mg.pl` to new style

--- chart_mg.pl
+++ chart_mg.pl
@@ -1,57 +1,60 @@
 #!/usr/bin/perl
 use strict;
 use warnings;
+use autodie;
 
-use Getopt::Long;
-use Pod::Usage;
+use Getopt::Long qw(HelpMessage);
 use Config::Tiny;
+use FindBin;
 use YAML qw(Dump Load DumpFile LoadFile);
 
 use AlignDB::Excel;
 use AlignDB::Stopwatch;
 
-use FindBin;
-
 #----------------------------------------------------------#
 # GetOpt section
 #----------------------------------------------------------#
-my $Config = Config::Tiny->new;
-$Config = Config::Tiny->read("$FindBin::Bin/../alignDB.ini");
+my $Config = Config::Tiny->read("$FindBin::Bin/config.ini");
 
-# stat parameter
-my $jc_correction   = $Config->{stat}{jc_correction};
-my $time_stamp      = $Config->{stat}{time_stamp};
-my $add_index_sheet = $Config->{stat}{add_index_sheet};
+# record ARGV and Config
+my $stopwatch = AlignDB::Stopwatch->new(
+    program_name => $0,
+    program_argv => [@ARGV],
+    program_conf => $Config,
+);
 
-my $add_trend = 0;
+=head1 NAME
 
-my $infile  = '';
-my $outfile = '';
+chart_mg.pl - Use Win32::OLE to automate Excel chart
 
-my %replace;
+=head1 SYNOPSIS
 
-my $man  = 0;
-my $help = 0;
+    perl chart_mg.pl [options]
+      Options:
+        --help            brief help message
+        --man             full documentation
+        --infile          input file name (full path)
+        --outfile         output file name
+
+=cut
 
 GetOptions(
-    'help|?'            => \$help,
-    'man'               => \$man,
-    'infile=s'          => \$infile,
-    'outfile=s'         => \$outfile,
-    'jc=s'              => \$jc_correction,
-    'time_stamp=s'      => \$time_stamp,
-    'add_index_sheet=s' => \$add_index_sheet,
-    'replace=s'         => \%replace,
-    'add_trend=s'       => \$add_trend,
-) or pod2usage(2);
+    'help|?'      => sub { HelpMessage(0) },
+    'infile|i=s'  => \( my $infile ),
+    'outfile|o=s' => \( my $outfile ),
+    'jc=s'              => \( my $jc_correction   = $Config->{stat}{jc_correction} ),
+    'time_stamp=s'      => \( my $time_stamp      = $Config->{stat}{time_stamp} ),
+    'add_index_sheet=s' => \( my $add_index_sheet = $Config->{stat}{add_index_sheet} ),
+    'replace=s'         => \( my %replace ),
+    'add_trend=s'       => \( my $add_trend ),
+) or HelpMessage(1);
 
-pod2usage(1) if $help;
-pod2usage( -exitstatus => 0, -verbose => 2 ) if $man;
+# die unless we got the mandatory argument
+HelpMessage(1) unless defined $infile;
 
 #----------------------------------------------------------#
 # Init section
 #----------------------------------------------------------#
-my $stopwatch = AlignDB::Stopwatch->new;
 $stopwatch->start_message("Processing $infile...");
 
 my $excel_obj;
@@ -70,10 +73,6 @@ else {
     $outfile = $excel_obj->outfile;
 }
 
-#----------------------------------------------------------#
-# START
-#----------------------------------------------------------#
-# jc
 $excel_obj->jc_correction if $jc_correction;
 
 #----------------------------------------------------------#
@@ -149,7 +148,7 @@ $excel_obj->jc_correction if $jc_correction;
     $option{chart_serial} = 1;
     $option{y_column}     = 2;
     $option{last_row}     = 32;
-    $option{x_max_scale}     = 30;
+    $option{x_max_scale}  = 30;
     $option{x_title}      = "Gradient";
     $option{y_title}      = "Window GC";
     $option{Top}          = 14.25;
@@ -242,37 +241,3 @@ $stopwatch->end_message;
 exit;
 
 __END__
-    
-=head1 NAME
-
-    gc_chart_factory.pl - Use Win32::OLE to automate Excel chart
-
-=head1 SYNOPSIS
-
-    gc_chart_factory.pl [options]
-      Options:
-        --help            brief help message
-        --man             full documentation
-        --infile          input file name (full path)
-        --outfile         output file name
-
-=head1 OPTIONS
-
-=over 8
-
-=item B<-help>
-
-Print a brief help message and exits.
-
-=item B<-man>
-
-Prints the manual page and exits.
-
-=back
-
-=head1 DESCRIPTION
-
-B<This program> will read the given input file(s) and do someting
-useful with the contents thereof.
-
-=cut
